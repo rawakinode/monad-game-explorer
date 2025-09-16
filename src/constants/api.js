42481e48a4116c6b4c858710e442faf366a1674c
Rawakinodes <59562911+rawakinode@users.noreply.github.com>
2025-09-16 10:42:31 +0800

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-// export const API_BASE = "https://monad-game-explorer-api.vercel.app";
+export const API_BASE = "https://monad-game-explorer-api.vercel.app";
 export const API_USERNAME = "https://www.monadclip.fun";
-export const API_BASE = "http://localhost:3000";
+// export const API_BASE = "http://localhost:3000";
